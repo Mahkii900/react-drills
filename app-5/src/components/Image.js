16b132d51e8e3d5c51ef0bff6c862585fc7cc95f
Mahkii900 <col.graff.battleschool@gmail.com>
2019-07-09 16:16:50 -0600

    finished problem 5

---
+++ app-5/src/components/Image.js
@@ -0,0 +1,15 @@
+import React, {Component} from 'react'
+
+export default class Image extends Component {
+    constructor(props) {
+        super(props)
+    }
+
+    render() {
+        return (
+            <div>
+              <img src={this.props.picSrc}/>
+            </div>
+        )
+    }
+}
