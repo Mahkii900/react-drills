4e5c68f667620287f931ef8d68625825a5679aef
Mahkii900 <col.graff.battleschool@gmail.com>
2019-07-09 14:51:14 -0600

    finished problem 1

--- app-1/src/App.js
+++ app-1/src/App.js
@@ -1,20 +1,30 @@
 import React, { Component } from "react";
-import logo from "./logo.svg";
 import "./App.css";
 
 class App extends Component {
+  constructor() {
+    super()
+
+    this.state = {
+      text: ''
+    }
+  }
+
+  changeHandler(input) {
+    this.setState({text: input})
+  }
+
   render() {
     return (
       <div className="App">
         <div className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h2>Welcome to React</h2>
+          <input onChange={(e) => this.changeHandler(e.target.value)}/>
+          <div>
+            <h4>{this.state.text}</h4>
+          </div>
         </div>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
       </div>
-    );
+    )
   }
 }
 
